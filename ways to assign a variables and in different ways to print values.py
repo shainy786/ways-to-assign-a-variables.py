442fa9eff1c9503789349e5a43dd585df92de743
Shainaz <102401756+shainy786@users.noreply.github.com>
2022-09-19 02:28:14 -0700

    Create ways to assign a variables and in different ways to print values.py

---
+++ ways to assign a variables and in different ways to print values.py
@@ -0,0 +1,54 @@
+#"1st way assign a variable"and "ways to print values"
+a=12
+b=23
+c=68
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+print(b,c,a)
+print(a);print(b);print(c)
+print(a);print(b);print(c)
+#"2nd way assign a variable" and "ways to print values"
+a=12;b=23;c=68
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+print(b,c,a)
+print(a);print(b);print(c)
+print(a);print(b);print(c)
+#"3nd way assign a variable" and "ways to print values"
+a,b,c=12,23,68
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+print(b,c,a)
+print(a);print(b);print(c)
+print(a);print(b);print(c)
+# a,b,c,d=1,2,3
+# a,b,c=1,2,3,4
+# print(a,b,c,d)                   #not enough values to unpack
+# print(a,b,c)                        #too many values to unpack 
+#"4th way assign a variable" and "ways to print values"
+a=b=c=d=e=1
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(a,b,c,d,e)
+print(a);print(b);print(c);print(d);print(e)
+print(a),print(b),print(c),print(d),print(e)
+#"5th way assign a variable" and "ways to print values"
+a=b=c=3;d=e=8
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(a,b,c,d,e)
+print(a,b,c),print(d,e)
+print(a);print(b);print(c);print(d);print(e)
+print(a),print(b),print(c),print(d),print(e)
